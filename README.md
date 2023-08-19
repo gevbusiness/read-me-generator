8ccf9601d46bf32a9f07da12549c3f70320f2ea1
gevbusiness <ggtherrappack@yahoo.com>
2023-08-19 11:49:08 -0700

    created everything

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# read-me-generator
+# sdfsdfsf
+
+
+  ## Description
+  
+  sdfsdfsdf
+
+  ## Table of Contents
+
+  * [Installation](#installation)
+
+  * [Usage](#usage)
+
+  * [Usage](#usage)
+
+  * [Contributing](#contributing)
+
+  * [Tests](#tests)
+
+  * [Questions](#questions)
+
+  ## Installation
+
+  To install necessary dependencies, run the following command:
+
+  ```
+  sdfsdfsd
+  ```
+
+  ## Usage
+
+  sdfsdfs
+
+  ## Contributing
+
+  sdfsdfsdf
+
+  ## Tests
+
+  To run tests, run the following command:
+
+  ```
+  npm test
+  ```
+
+  ## Questions
+
+  If you have any questions about the repo, open an issue or contact me directly at dsdfsdfs. You can find more of my work at [fdsdfsdf](https://github.com/fdsdfsdf/).
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,8 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -14,6 +16,52 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
+
+  ## Description
+  
+  ${data.description}
+
+  ## Table of Contents
+
+  * [Installation](#installation)
+
+  * [Usage](#usage)
+
+  * [Usage](#usage)
+
+  * [Contributing](#contributing)
+
+  * [Tests](#tests)
+
+  * [Questions](#questions)
+
+  ## Installation
+
+  To install necessary dependencies, run the following command:
+
+  \`\`\`
+  ${data.installation}
+  \`\`\`
+
+  ## Usage
+
+  ${data.usage}
+
+  ## Contributing
+
+  ${data.contributing}
+
+  ## Tests
+
+  To run tests, run the following command:
+
+  \`\`\`
+  ${data.test}
+  \`\`\`
+
+  ## Questions
+
+  If you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my work at [${data.github}](https://github.com/${data.github}/).
 `;
 }
 
